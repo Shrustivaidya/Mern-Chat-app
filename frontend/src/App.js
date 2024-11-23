055c2ec263a1c706d8b8c809f0dc3ae96f13b9aa
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-11-23 10:48:52 +0530

    Worked on basic setup to fetch API from backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-
-import React from 'react';
-
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { Route } from "react-router-dom";
+import Chatpage from "./Pages/Chatpage";
 
 function App() {
   return (
-    <div>
-     hello
+    <div className="App">
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
