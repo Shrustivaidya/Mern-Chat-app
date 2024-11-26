49fe363a23715dec904df17636a1872d67805b81
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-11-26 12:21:08 +0530

    Search user and create chat API

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 
@@ -16,7 +17,8 @@ app.get('/',(req,res)=>{
     res.send("API is running");
 });
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
