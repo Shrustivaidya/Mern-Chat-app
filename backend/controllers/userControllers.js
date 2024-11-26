709933bbd739142d1a1842388849b8d4ab81fb5c
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-11-26 10:44:08 +0530

    API for search functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,6 +4,25 @@ const generateToken = require("../config/generateToken")
 
 
 
+
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+
 //@description     Register new user
 //@route           POST /api/user/
 //@access          Public
@@ -67,4 +86,4 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
-module.exports = { registerUser, authUser};
+module.exports = { registerUser, authUser,allUsers};
